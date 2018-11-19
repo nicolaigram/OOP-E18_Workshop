7248351ec454080e73eb7572e0ec8578ef44adc7
Nicolai Gram <nicolai@brnd.com>
2018-11-19 14:19:44 +0100

    Implement BuildingDatabase from UML

--- src/domain/BuildingDatabase.java
+++ src/domain/BuildingDatabase.java
@@ -1,5 +1,27 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class BuildingDatabase {
-    
+
+    private ArrayList<Building> buildings;
+
+    public BuildingDatabase() {
+        buildings = new ArrayList<>();
+    }
+
+    public void addBuilding(Building building) {
+        buildings.add(building);
+    }
+
+    public void removeBuilding(Building building) {
+        buildings.remove(building);
+    }
+
+    public void getBuilding(UUID id) {
+        for (Building building : buildings) {
+            // If building.getId == id, then remove from arraylist.
+        }
+    }
 }
