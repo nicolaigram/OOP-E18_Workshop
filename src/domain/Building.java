6f81b624978faf81a49a7af8b99b8b526f4702e6
Nicolai Gram <nicolai@brnd.com>
2018-11-19 14:57:09 +0100

    Work on Building class

--- src/domain/Building.java
+++ src/domain/Building.java
@@ -1,16 +1,43 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Building {
     private UUID id;
     private String name;
     private String location;
+    private ArrayList<Sensor> sensors;
+    private ArrayList<Actuator> actuators;
+
+    public Building(String name, String location) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.location = location;
+        sensors = new ArrayList<>();
+        actuators = new ArrayList<>();
+    }
     
-    public void printSensors() {
-        
+    public void addSensor(Sensor sensor) {
+        sensors.add(sensor);
     }
-    public void printActuators(){
-        
+    
+    public void addActuator(Actuator actuator) {
+        actuators.add(actuator);
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    
+    
+    
 }
